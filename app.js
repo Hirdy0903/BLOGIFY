eb3f6209fbbd08ba704b267591fab179aba79e6e
Hirdyansh <hirdyanshsaxena7@gmail.com>
2026-05-20 01:07:37 +0530

    port updated

---
+++ app.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+
+require('dotenv').config();
+
+const { router: userRoute } = require('./routes/user');
+const { router: blogRoute } = require('./routes/blog');
+const { checkForAuthenticatioInCookiee } = require('./middleware/authentication');
+const { Blog } = require('./models/blogs');
+
+const app = express();
+
+const port = process.env.PORT||9987;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(checkForAuthenticatioInCookiee('token'));
+
+app.use((req, res, next) => {
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} ${req.path}`
+  );
+
+  next();
+});
+
+app.set('view engine', 'ejs');
+
+app.set('views', path.resolve('./views'));
+
+app.get('/', async (req, res) => {
+
+  const allBlogs = await Blog.find({});
+
+  return res.render('homepage', {
+    user: req.user,
+    blogs: allBlogs,
+  });
+
+});
+
+app.use('/user', userRoute);
+
+app.use('/blog', blogRoute);
+
+if (require.main === module) {
+
+  mongoose
+    .connect(process.env.URI)
+
+    .then(() => {
+      console.log('Connected to DB');
+    })
+
+    .catch((err) => {
+      console.error('MongoDB connection error:', err);
+    });
+
+  app.listen(port, () => {
+    console.log('server is running on port ' + port);
+  });
+
+}
+
+module.exports = { app };
